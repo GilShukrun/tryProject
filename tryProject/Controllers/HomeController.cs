8e77763bd54198b23c3acb991d8d844502c25583
YardenMirko <yardenmirko@gmail.com>
2021-05-21 15:10:50 +0300

    Fixed

--- tryProject/Controllers/HomeController.cs
+++ tryProject/Controllers/HomeController.cs
@@ -19,7 +19,6 @@ namespace tryProject.Controllers
         {
             _logger = logger;
         }
-        [Authorize]
         public IActionResult Index()
         {
             return View();
